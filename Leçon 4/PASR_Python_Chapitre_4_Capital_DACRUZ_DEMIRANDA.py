f7b6f35c24ca2272528374068b3ed6417f773cc6
Alexy DA CRUZ (GeomTech) <dacruzalexy@gmail.com>
2021-01-07 21:08:28 +0100

    Modification script capital

--- Leçon 4/PASR_Python_Chapitre_4_Capital_DACRUZ_DEMIRANDA.py
+++ Leçon 4/PASR_Python_Chapitre_4_Capital_DACRUZ_DEMIRANDA.py
@@ -6,33 +6,38 @@
 # Date : 07/01/2021
 # Thème du script : Multiplier son capital
 
+def auRevoir():
+    """
+    affiche 3 sauts de ligne et Fin du programme
+    Entrées : aucune
+    Sortie : aucune
+    """
+    print("\n\n\nAu revoir\n\nFin du programme")
 
-# Initialisation des variables
-capitalInitial = 0
-taux = 0
-multiplicateur = 0
-capitalFinalAttendu = 0
-capitalFinal = 0
-nbreAnnees = 0
+def CalculCapital(capital, taux, multiplicateur):
+    nbreAnnees = 0
+    capitalAttendu = capital * multiplicateur # Calcul du capital final attendu par l'utilisateur
 
-capitalInitial = float(input("Quel est le capital initial ? "))  # Demande à l'utilisateur le capital initial
+    while (capital < capitalAttendu ) : # Augmentation du capital par le taux spécifié tant que le capital n'a pas dépassé l'attente de l'utilisateur
+        capital = capital + (capital * taux)  # Calcul du capital final en y ajoutant le taux spécifié par l'utilisateur
+        nbreAnnees += 1 # Comptage des années
+
+    capital = round(capital, 2)  # Arrondir a deux décimal la variable capital
+    return [nbreAnnees, capital]
+
+capitalInitial = float(input("Quel est le capital initial ? ")) # Demande à l'utilisateur le capital initial
 taux = float(input("Quel est le taux d'intérêt annuel (Pour 2%, on attend 0.02) ? ")) # Demande à l'utilisateur le taux d'interet
-multiplicateur = float(input("Pour quel coefficient multiplicateur voulez vous avoir un prévision ? ")) # Demande à l'utilisateur le coefficient multiplicateur 
+multiplicateur = float(input("Pour quel coefficient multiplicateur voulez vous avoir un prévision ? ")) # Demande à l'utilisateur le coefficient multiplicateur
 
-capitalFinalAttendu = capitalInitial * multiplicateur # Calcul du capital final attendu par l'utilisateur
-capitalFinal = capitalInitial  # Mise à jour de la variable capitalFinal
+capital = CalculCapital(capitalInitial, taux, multiplicateur) # Appelle fonction
 
-while (capitalFinal < capitalFinalAttendu ) : # Augmentation du capital par le taux spécifié tant que le capital n'a pas dépassé l'attente de l'utilisateur
-    capitalFinal = (capitalFinal + (capitalFinal * taux))  # Calcul du capital final en y ajoutant le taux spécifié par l'utilisateur
-    nbreAnnees += 1 # Comptage des années
-
-taux = taux*100 # Passage du taux en pourcentage
-capitalInitial = round (capitalInitial) # Arrondir a deux décimal la variable capitalInitial
-capitalFinal = round (capitalFinal) # Arrondir a deux décimal la variable capitalFinal
+taux = taux * 100 # Passage en %
+capitalInitial = round(capitalInitial, 2) # arrondi à deux décimal
 
 # Affichage du resultat
-print ( "Capital initial :" ,capitalInitial, 
-        "\n Taux :" ,taux, "%"
-        "\n Apres" ,nbreAnnees, "ans, votre capital aura été multiplié par" ,multiplicateur,
-        "\n Vous disposerez alors de :" ,capitalFinal
-        )
+print( "Capital initial :" , capitalInitial,
+        "\nTaux :" , taux, "%"
+        "\nApres" , capital[0], "ans, votre capital aura été multiplié par" , multiplicateur)
+print("Vous disposerez alors de :", capital[1])
+
+auRevoir()  # On appelle la fonction pour quitter le programme
