9dd733657cf2a29410e0a60c4ffcf924b8528b1f
Dylan <dylandemiranda@gmail.com>
2021-01-26 19:19:00 +0100

    Update PASR_Python_Chapitre_5_Supprimer_Voyelle_DACRUZ_DEMIRANDA.py

--- Leçon 5/PASR_Python_Chapitre_5_Supprimer_Voyelle_DACRUZ_DEMIRANDA.py
+++ Leçon 5/PASR_Python_Chapitre_5_Supprimer_Voyelle_DACRUZ_DEMIRANDA.py
@@ -6,26 +6,31 @@
 # Date : 26/01/2021
 # Thème du script : Supprimer Voyelle
 
+# Initialisation des variables
 V = ""
 L = []
 
 def supprimerVoyelle(listeMot):
- 
-    for mot in listeMot:
-        if (V in mot):
-            listeMot.remove(mot)
+    """
+    affiche une liste de mot dans laquelle on a retirer 
+    les mots contenant une voyelle sélectionnée
+    Entrée : liste de mot
+    Sortie : affiche la liste de mot sans la voyelle sélectionnée
+    """
+    for mot in listeMot: # Pour chaque mot de la liste:
+        if (V in mot): # Si l'on trouve la voyelle sélectionnée:
+            listeMot.remove(mot) # Suppression du mot contenant la voyelle sélectionnée.
         
-    print(listeMot)
+    print(listeMot) # Affichage de la liste modifiée
 
 
+nbreValeurs= int(input("Combien de valeurs à ajouter dans la liste ?")) # Demande à l'utilisateur d'entrer la longueur de la liste
 
-nbreValeurs= int(input("Combien de valeurs à ajouter dans la liste ?"))
-
-for i in range(0, nbreValeurs):
-    nouveauMot = str(input("Ajouter un mot:"))
-    L.append(nouveauMot)
+for i in range(0, nbreValeurs): # Boucle for permettant à l'utilisateur de renseigner autant de mot que la longueur de la liste
+    nouveauMot = str(input("Ajouter un mot:")) 
+    L.append(nouveauMot)  # ajout du nouveau mot à la liste
     
-V = str(input("Supprimer les mots contenant la voyelle:"))
+V = str(input("Supprimer les mots contenant la voyelle:")) # Demande à l'utilisateur de choisir la voyelle pour supprimer les mots contenant celle-ci
 
 
-supprimerVoyelle(L)
+supprimerVoyelle(L)  # Appelle la fonction "supprimerVoyelle" avec la liste L
