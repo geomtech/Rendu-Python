f134ab8e82a89f9ed66d700f04e6d19dcbc4a732
Alexy DA CRUZ <dacruzalexy@gmail.com>
2021-02-04 19:15:28 +0100

    Update PASR_Python_Chapitre_6_Fichiers_Exo4_DACRUZ_DEMIRANDA.py

--- Leçon 6/PASR_Python_Chapitre_6_Fichiers_Exo4_DACRUZ_DEMIRANDA.py
+++ Leçon 6/PASR_Python_Chapitre_6_Fichiers_Exo4_DACRUZ_DEMIRANDA.py
@@ -1,1 +1,80 @@
-{\rtf1}
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# Auteur(s) : Dylan DE MIRANDA (dylandemiranda@gmail.com) & Alexy DA CRUZ (adacruz@geomtech.fr)
+# Version : 0.1
+# Date : 04/02/2021
+# Thème du script : Analyse récursive des fichiers d'un répertoire
+
+import os
+import time
+
+def goodBye():
+    """
+    affiche 3 sauts de ligne et Fin du programme
+    Entrées : aucune
+    Sortie : aucune
+    """
+    print("\n\n\nAu revoir\n\nFin du programme")
+
+def analyseFile(file_path, word_to_find):
+    """
+    """
+    if (os.path.exists(file_path)):
+        word_founded = False
+        lines_with_word = []
+
+        try: # On essaie
+            f = open(file_path, 'r', encoding="utf-8") # On ouvre le fichier demandé
+            file_lines = f.readlines() # On liste les lignes
+
+            lines_counter = 1 # On créer un compteur à partir de 1 car un fichier n'a pas de ligne 0 :-)
+            for line in file_lines: # Pour chaque ligne du fichier
+                if line.startswith(word_to_find): # SI la ligne commence par le mot recherché                    
+                    line_str = str(lines_counter) + ":" + line
+                    lines_with_word.append(line_str)
+                    word_founded = True
+                
+                lines_counter += 1 # On compte les lignes
+        except IOError: # Si une erreur de type IO survient
+            print("Erreur : Le fichier", file_path, "n'est pas accessible") # On affiche un message d'erreur
+        finally:
+            f.close() # On ferme le fichier
+
+        return [word_founded, lines_with_word]
+
+def analyseDirectory(directory_path, word_to_find):
+    """
+    """    
+
+    directory_content = os.listdir(directory_path) # On liste les fichiers
+
+    for content in directory_content: # Pour chaque fichier ou répertoire dans le répertoire
+        content_path = os.path.join(directory_path, content)
+
+        if (os.path.isfile(content_path)): # Si c'est un fichier
+            result_analyse = analyseFile(content_path, word_to_find) # On analyse le fichier
+
+            if (result_analyse[0]):
+                print("\nDossier :", directory_path, "\n")
+                print("\n", os.path.basename(content_path), sep="")  # On affiche le nom du fichier
+                for line in result_analyse[1]:
+                    print(line.rstrip('\n'))
+        elif (os.path.isdir(content_path)): # Si c'est un répertoire
+            analyseDirectory(content_path, word_to_find) # On analyse le répertoire
+
+################################################
+############### MAIN PROGRAM  ##################
+################################################
+
+directory_to_analyse = input("Répertoire à analyser > ")
+directory_path = os.path.join(os.getcwd(), directory_to_analyse)
+
+if (os.path.exists(directory_path)):  # Si le répertoire existe
+    word_to_find = input("Mot à trouver > ")    
+
+    analyseDirectory(directory_path, word_to_find)
+else:
+    print("Ce répertoire n'existe pas")
+
+goodBye()
