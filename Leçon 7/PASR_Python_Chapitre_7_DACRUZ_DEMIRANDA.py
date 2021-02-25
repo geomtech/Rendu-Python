e395f1fd64fc71e9c42b2d59ba545a384adebdda
Alexy DA CRUZ <dacruzalexy@gmail.com>
2021-02-25 09:46:47 +0100

    Ajout script 7

---
+++ Leçon 7/PASR_Python_Chapitre_7_DACRUZ_DEMIRANDA.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# Auteur(s) : Dylan DE MIRANDA (dylandemiranda@gmail.com) & Alexy DA CRUZ (adacruz@geomtech.fr)
+# Version : 0.1
+# Date : 26/01/2021
+# Thème du script : Effectifs cumulés d'une liste :
+#                   Ecrire un programme qui attend une liste L d'entiers et qui affiche les effectifs cumulés des
+#                   différents éléments de la liste sous forme de bâtons constitués d'étoiles.
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
