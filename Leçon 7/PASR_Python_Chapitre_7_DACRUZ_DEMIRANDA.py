92e502661176e324abe7448689bb83d5926a2f78
Alexy DA CRUZ <dacruzalexy@gmail.com>
2021-02-25 10:42:18 +0100

    Update PASR_Python_Chapitre_7_DACRUZ_DEMIRANDA.py

--- Leçon 7/PASR_Python_Chapitre_7_DACRUZ_DEMIRANDA.py
+++ Leçon 7/PASR_Python_Chapitre_7_DACRUZ_DEMIRANDA.py
@@ -3,13 +3,38 @@
 
 # Auteur(s) : Dylan DE MIRANDA (dylandemiranda@gmail.com) & Alexy DA CRUZ (adacruz@geomtech.fr)
 # Version : 0.1
-# Date : 26/01/2021
-# Thème du script : Effectifs cumulés d'une liste :
-#                   Ecrire un programme qui attend une liste L d'entiers et qui affiche les effectifs cumulés des
-#                   différents éléments de la liste sous forme de bâtons constitués d'étoiles.
+# Date : 25/02/2021
+# Thème du script : Dictionnaire affiché en partant de la définition la plus longue.
 
-def main():
+def sort_dict(dict_words: dict) -> list:
+    sorted_words = []
+
+    for key, value in dict_words.items():
+        for definition in dict_words.values():
+            print(len(value), len(definition))
+            if (len(value) >= len(definition)):
+                print(key)
+
+    return sorted_words
+
+def display_dict(sorted_list: list) -> None:
     pass
+    #print(sorted_list)
+
+def main() -> None:
+    original_dict = {"Jeux": "On peut définir le jeu comme une activité de loisirs d'ordre physique ou bien psychique, soumise à des règles conventionnelles, à laquelle on s'adonne pour se divertir, tirer du plaisir et de l'amusement.",
+            "Bouteille": "Récipient allongé à goulot étroit, en verre, en plastique, en métal ou en terre destiné aux liquides, notamment aux boissons",
+            "Montre": "Une montre est un instrument de mesure du temps qui se porte sur soi.",
+            "Pied": "Partie terminale du membre inférieur, articulée avec la jambe, permettant l'appui au sol dans la station debout et la marche.",
+            "Table": "Meuble composé d'un plateau horizontal reposant sur un ou plusieurs pieds ou supports. Servant notamment pour les repas",
+            "Chaise": "Siège à dossier, sans bras",
+            "Chaise2": "Siège à dossier, sans pied",
+            "Fauteuil": "Siège à dossier et à bras pour une personne",
+            "Commode": "Meuble à hauteur d'appui garni, le plus souvent, de tiroirs",
+            "Loup": "Animal feroce"}
+    
+    sorted_dict = sort_dict(original_dict)
+    display_dict(sorted_dict)
 
 if __name__ == "__main__":
     main()
